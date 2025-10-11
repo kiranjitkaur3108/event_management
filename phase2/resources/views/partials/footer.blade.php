8493c9699a2a6b26ea7bfeee3e0dba18b2f6b4aa
Sonali <sonali@gmail.com>
2025-10-11 02:26:17 -0400

    Modified

--- phase2/resources/views/partials/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="bg-dark text-light py-4 mt-5">
-    <div class="container d-flex flex-column flex-md-row justify-content-between align-items-center">
-        <div>&copy; {{ date('Y') }} CELEBRATIONS. All rights reserved.</div>
-        <div class="mt-2 mt-md-0">
-            <a class="text-light text-decoration-none me-3" href="/privacy">Privacy</a>
-            <a class="text-light text-decoration-none" href="/terms">Terms</a>
-        </div>
-    </div>
-</footer>
