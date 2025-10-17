0bd194bbbc28b5c2ffced7457c540d81ab2f1531
Sonali <sonali@gmail.com>
2025-10-17 17:41:28 -0400

    added contacts

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'password' => $request->password,
             'role' => 'user',
         ]);
 
